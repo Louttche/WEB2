81d6ce49deec783e7db4970792a71d4608cd2e78
kathy <lnicolaidou.katerina@gmail.com>
2020-03-09 19:10:04 +0100

    Update, ID Order Sort

--- app/src/app/tasks/tasks.component.ts
+++ app/src/app/tasks/tasks.component.ts
@@ -25,15 +25,21 @@ export class TasksComponent implements OnInit {
   }
 
   selectedTask: Task;
-  
 
   onSelect(task: Task): void {
     this.selectedTask = task;
   }
 
+  sortTasksID(): void{
+    for (let i = 0; i < this.tasks.length; i++) {
+      this.tasks[i].id = i + 1;      
+    }
+  }
+
   onCreate(taskDetails) {
     this.tasks.push({name: taskDetails.name, description: taskDetails.description, id: this.tasks.length + 1});
     this.TaskInfoForm.reset();
+    this.sortTasksID();
   }
 
   onDelete(task: Task) {
@@ -41,10 +47,14 @@ export class TasksComponent implements OnInit {
       this.tasks.splice(task.id - 1, 1);
     }
     console.log('Deleted task')
+    this.sortTasksID();
   }
 
-  onUpdate(taskDetails){
-    var index = this.tasks.findIndex(task => task === this.selectedTask);
-    console.log(index);
+  onUpdate(task: Task, taskDetails){
+      var index = this.tasks.findIndex(task => task === this.selectedTask);
+      this.tasks[index].name = taskDetails.name;
+      this.tasks[index].description = taskDetails.description;
+      this.TaskInfoForm.reset();
+      this.sortTasksID();
   }
 }
